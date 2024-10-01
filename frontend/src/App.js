4ac2e86977b91c7d5bd580aaf9d272cf3c80b38b
SilentDemon03 <serena.ooi03@gmail.com>
2024-10-01 16:23:59 +1000

    landing page UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
+
 function App() {
     
     return (
         <div className='page'>
-            <h1>Hello World!</h1>
+            <div className='image-section'>
+                <img src="/assets/istockphoto-1336490738-612x612.jpg" alt='Landing image'/>
+            </div>
+            <div className='text-section'>
+                <h1>Z-Habit</h1>
+                <p>Your personal habit tracker...</p>
+                <button className='start-button'>
+                    Get Started
+                    <img src="/assets/maki_arrow.svg" alt='arrow' className='arrow-icon'/>
+                </button>
+            </div>
         </div>
     );
 }
