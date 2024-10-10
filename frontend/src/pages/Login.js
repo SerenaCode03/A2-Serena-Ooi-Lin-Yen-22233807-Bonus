8a8879bb1726272da4e3bdca4100210057e09923
SilentDemon03 <serena.ooi03@gmail.com>
2024-10-10 16:32:38 +1100

    login page UI

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,7 +2,21 @@ import '../styles/Login.css';
 
 function Login(){
     return(
-        <h1>Welcome back !</h1>
+        <div className='login-page'>
+            <p className='welcome-text'>Welcome back !</p>
+            <div className='username-container'>
+                <img src="/assets/prime_user.svg" className="user-icon"/>
+                <input type="text" placeholder="Username" className='username-input' />
+            </div>
+            <div className='password-container'>
+                <img src="/assets/mdi_password-outline.svg" className="password-icon"/>
+                <input type="text" placeholder="Password" className='password-input' />
+            </div>
+            <button className='login-button'>
+                Login
+            </button>
+            <p className='signup-link'>Don't have an account?</p>
+        </div>
     );
 }
 export default Login;
